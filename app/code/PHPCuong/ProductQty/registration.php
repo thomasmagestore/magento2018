a5e44626952ca367b459dea5d3e3f2e165c18741
Thomas <doandinhdat.dhxd@gmail.com>
2018-02-14 18:23:01 +0700

    Commit code initial

---
+++ app/code/PHPCuong/ProductQty/registration.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @Author: Doan Dinh Dat <doandinhdat.dhxd@gmail.com>
+ * @Creation time: 2017-07-13 14:51:03
+ * @Last modified time: 2017-07-13 14:51:15
+ * @link: https://www.facebook.com/OnjinMatsui.VTC
+ *
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PHPCuong_ProductQty',
+    __DIR__
+);
